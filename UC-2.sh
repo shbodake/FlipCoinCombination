dd8f9ea3dc0e1752f05b4e661823ee0926c00c00
shbodake <sayalibodake28@gmail.com>
2021-09-16 13:08:34 +0530

    UC2 code determine percentage of singlet combination

---
+++ UC-2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Enter number of times to flip a combination:" 
+read nflips
+declare -A singlet=( [H]=0 [T]=0 )
+echo "---------------------------singlet_combination---------------------------"
+for(( flip = 1; flip <= nflips; flip++ ))
+do
+    echo "Flip-$flip is "
+    (( toss = RANDOM % 2 ))
+    case $toss in 
+        0)
+            echo "Heads"
+            (( singlet[H]++ ))
+            ;;
+        1)
+            echo "Tails"
+            (( singlet[T]++ ))
+    esac
+done
+
+for combination in ${!singlet[@]}
+do
+    percentage=$(( ${singlet[$combination]}  * 100 / nflips ))
+    singlet[$combination]=$percentage
+    echo "percentage of $combination is ${singlet[$combination]}%"
+done
