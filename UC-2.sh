e346aaacf23dcf77c2003644c44b244cd3643a05
shbodake <sayalibodake28@gmail.com>
2021-09-16 15:46:50 +0530

    UC4 code determine percentage of singlet combination

--- UC-2.sh
+++ UC-2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "Enter number of times to flip a combination:" 
+echo "Enter number of times to flip a combination:"
 read nflips
 declare -A singlet=( [H]=0 [T]=0 )
-echo "---------------------------singlet_combination---------------------------"
 for(( flip = 1; flip <= nflips; flip++ ))
 do
     echo "Flip-$flip is "
